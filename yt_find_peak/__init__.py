f12ee3febe4bf98da6ef5353fd344d86795c17b8
nssuperx <40332687+nssuperx@users.noreply.github.com>
2024-10-20 14:22:44 +0900

    ヒートマップからピーク取得

--- yt_find_peak/__init__.py
+++ yt_find_peak/__init__.py
@@ -1,1 +1,1 @@
-from yt_find_peak.yt_find_peak import find_peak_sound, find_peak_live_chat  # noqa
+from yt_find_peak.yt_find_peak import find_peak_sound, find_peak_live_chat, find_peak_heatmap  # noqa
--- yt_find_peak/yt_find_peak.py
+++ yt_find_peak/yt_find_peak.py
@@ -12,7 +12,7 @@ def find_peak_sound(filename: str, point: int) -> tuple[int, ...]:
         point (int): ピーク値取得数
 
     Returns:
-        tuple[int, ...]: 見つけたピーク時刻
+        tuple[int, ...]: 音量が大きい時刻
     """
     rate, sound = wavfile.read(f"{filename}.wav")
     sound_l_abs = np.abs(sound[:, 0])
@@ -25,20 +25,18 @@ def find_peak_sound(filename: str, point: int) -> tuple[int, ...]:
 
 
 def find_peak_live_chat(filename: str, point: int) -> tuple[int, ...]:
-    """YouTubeのライブチャットのjsonからピークを見つける
+    """YouTubeのライブチャットのjsonからコメント数が多い時刻を見つける
 
     Args:
         filename (str): 拡張子抜きのファイル名(live_chat.jsonファイルを想定)
         point (int): ピーク値取得数
 
     Returns:
-        tuple[int, ...]: 見つけたピーク時刻
+        tuple[int, ...]: コメント数が多い時刻
     """
     with open(f"{filename}.live_chat.json", encoding="utf-8") as f:
         chat_jsons = (json.loads(line) for line in f)
-        timestamps = [
-            t.get("replayChatItemAction", {}).get("videoOffsetTimeMsec", 0) for t in chat_jsons
-        ]
+        timestamps = [t.get("replayChatItemAction", {}).get("videoOffsetTimeMsec", 0) for t in chat_jsons]
     timestamps = np.array(timestamps, dtype=np.int32)
     # 0は配信開始前だから抜く
     timestamps = timestamps[timestamps > 0]
@@ -55,3 +53,37 @@ def find_peak_live_chat(filename: str, point: int) -> tuple[int, ...]:
     # ピーク値が大きい順にする
     desc = np.argsort(counts[peaks])[::-1]
     return tuple(int(i) for i in peaks[desc][:point])
+
+
+def find_peak_heatmap(filename: str, point: int) -> tuple[int, ...]:
+    """youtube-dlの--write-info-jsonして取得できるファイルから
+    ヒートマップの値を見て大きい時刻を取得する
+
+    Args:
+        filename (str): 拡張子抜きのファイル名(info.jsonファイルを想定)
+        point (int): ピーク値取得数
+
+    Returns:
+        tuple[int, ...]: ヒートマップの値が大きい時刻
+    """
+    with open(f"{filename}.info.json", encoding="utf-8") as f:
+        heatmap = json.load(f)["heatmap"]
+
+    # 一回全部ndarrayにする
+    raw = np.array(
+        [(float(v.get("start_time", 0)), float(v.get("end_time", 0)), float(v.get("value", 0))) for v in heatmap],
+        dtype=np.float32,
+    )
+
+    # 最初はvalueが1だから除外
+    raw = raw[1:]
+    # start_time < end_time かつ heatmapの値が0より大きいなら有効（これでFalseになる値はほぼないと思うけど）
+    valid = raw[(raw[:, 0] < raw[:, 1]) & (raw[:, 2] > 0.0)]
+    time_value = np.array([(valid[:, 1] + valid[:, 0]) * 0.5, valid[:, 2]]).T
+
+    peaks, _ = find_peaks(time_value[:, 1])
+
+    # ピーク値が大きい順にする
+    peak_time_value = time_value[peaks]
+    desc = np.argsort(peak_time_value[:, 1])[::-1]
+    return tuple(int(i) for i in peak_time_value[:, 0][desc][:point])
